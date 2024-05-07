9633ddcdd46cf9dc0867cbd391197c5b33ef2fc8
MrT0mX <mdratulhassan10@gmail.com>
2024-05-07 17:04:06 +0600

    Create MTX.js

---
+++ MTX.js
@@ -0,0 +1,13 @@
+//_______________________ ┏  Info  ┓ _______________________\\
+//
+//   Edited BY: MrTomXxX
+//   
+//   -----------Note ------------
+//
+//   Api Owner: Bot
+//   Don't sell this API,
+//   Don't Change this text,
+//   Who wants to upload don't forget to credit :),
+//   Who does not place credit will take action
+//   
+//_______________________ ┏ Modified By MrTomXxX ┓ _______________________\\
